cd7e5a026af16ac540c04ef0042f16cc5d9d798f
IorMemorE <None>
2024-07-08 20:12:00 +0800

    Upload Assets

---
+++ assets/display/player_api.js
@@ -0,0 +1,22 @@
+(function () {
+    const infoProvider = {
+        getPlayerNames() {
+            return parent.playerNames
+        },
+        setSize(width, height) {
+            const iframe = window.frameElement
+            iframe.height = height
+            iframe.width = width
+        },
+        notifyInitComplete() {
+            parent.callbacks.initcomplete()
+        },
+        setDisplayCallback(cb) {
+            parent.callbacks.display = cb
+        },
+        setGameOverCallback(cb) {
+            parent.callbacks.gameover = cb
+        },
+    }
+})()
+
